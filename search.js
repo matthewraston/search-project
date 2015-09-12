4802c9b5ad9f86e2a07c9471ddd0ff8494ccdda3
timothy-long <timothy_long@hotmail.com>
2015-09-12 18:55:38 +0100

    Added promises to search.
    All search sources now happen asynchronously, and wait until all sources
    have completed before returning search results.
    Had the side effect of also splitting up the code for each source into
    separate functions.

--- search.js
+++ search.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var sponsoredGifs = require('./sponsoredgifs.js').sponsoredGifs;
+var Promise = require('promise');
 
 // may need a different key?
 var YouTube = require('youtube-node');
@@ -15,57 +16,72 @@ var exclusionList = [
 
 module.exports = {
     query: function (query, callback) {
-        var gifSearch = query.replace(/ /g, '+');
-        var gifResults = [];
-        var majesticSearchResults = [];
-        var sponsoredResults = [];
-        var sponsoredGifResults = [];
-        var youtubeResults = [];
-        var numGifResults = 6;
+        Promise.all([
+            getSponsoredGifsPromise(query),
+            getGiphyPromise(query),
+            getYoutubePromise(query),
+            getMajesticPromise(query),
+        ])
+        .then(function(res) {
+            return callback({
+                sponsoredResults: res[0],
+                gifResults: res[1],
+                youtubeResults: res[2],
+                majesticSearchResults: res[3],
+            });
+        });
+    }
+}
 
+function getSponsoredGifsPromise(query)
+{
+    return new Promise(function(resolve, reject) {
         // check the search term against sponsored gifs...
         var searchComponents = query.split(" ");
+        var sponsoredResults = [];
+        var sponsoredGifResults = [];
 
         gifsloop: for (var i = 0; i < sponsoredGifs.length; i++) {
-                var tags = sponsoredGifs[i]["tags"].split(", ");
-                searchtermloop: for (var j = 0; j < searchComponents.length; j++) {
-                    // next loop if search component is in the exclusion list
-                    if (exclusionList.indexOf(searchComponents[j].toLowerCase()) > -1) {
-                        continue searchtermloop;
-                    }
+            var tags = sponsoredGifs[i]["tags"].split(", ");
+            searchtermloop: for (var j = 0; j < searchComponents.length; j++) {
+                // next loop if search component is in the exclusion list
+                if (exclusionList.indexOf(searchComponents[j].toLowerCase()) > -1) {
+                    continue searchtermloop;
+                }
 
-                    // task - more occurences in tags, more search relevence
-                    tagsloop: for (var k = 0; k < tags.length; k++) {
-                        // match - add to gif results, next gif loop
-                        if (searchComponents[j].toLowerCase() == tags[k].toLowerCase()) {
-                            // search sponsoredResults to see if the gif has already been found
-                            // and add 1 to its score
-                            var exists = 0;
+                // task - more occurences in tags, more search relevence
+                tagsloop: for (var k = 0; k < tags.length; k++) {
+                    // match - add to gif results, next gif loop
+                    if (searchComponents[j].toLowerCase() == tags[k].toLowerCase()) {
+                        // search sponsoredResults to see if the gif has already been found
+                        // and add 1 to its score
+                        var exists = 0;
 
-                            for (var l = 0; l < sponsoredResults.length; l++) {
-                                var gif = sponsoredResults[l];
-                                if (gif.url == sponsoredGifs[i]["url"]) {
-                                    gif.score++;
-                                    gif.tags += " " + tags[k].toLowerCase();
-                                    exists = 1;
-                                }
+                        for (var l = 0; l < sponsoredResults.length; l++) {
+                            var gif = sponsoredResults[l];
+                            if (gif.url == sponsoredGifs[i]["url"]) {
+                                gif.score++;
+                                gif.tags += " " + tags[k].toLowerCase();
+                                exists = 1;
                             }
+                        }
 
-                            if (!exists) {
-                                // otherwise, create a new result
-                                var gif = ({
-                                    url: sponsoredGifs[i]["url"],
-                                    score: 1,
-                                    tags: tags[k].toLowerCase()
-                                });
+                        if (!exists) {
+                            // otherwise, create a new result
+                            var gif = ({
+                                url: sponsoredGifs[i]["url"],
+                                score: 1,
+                                tags: tags[k].toLowerCase()
+                            });
 
-                                sponsoredResults.push(gif);
-                            }
+                            sponsoredResults.push(gif);
                         }
                     }
                 }
             }
-            // order by score
+        }
+
+        // order by score
         sponsoredResults.sort(function (a, b) {
             if (a.score > b.score) {
                 return -1;
@@ -83,7 +99,14 @@ module.exports = {
         } else {
             sponsoredGifResults = sponsoredResults.splice(0, sponsoredResults.length);
         }
+        
+        resolve(sponsoredGifResults);
+    });
+}
 
+function getMajesticPromise(query)
+{
+    return new Promise(function(resolve, reject) {
         // do a majestic search
         http.get("http://api.majestic.com/api/json?app_api_key=CD7525F601EA2CE773BDD24A220358C1&cmd=SearchByKeyword&count=5&datasource=fresh&scope=2&query=" + query, function (res) {
             var data = "";
@@ -93,53 +116,73 @@ module.exports = {
             });
 
             res.on('end', function () {
-                getMajesticDataSuccess(data);
-            });
-        }).on('error', function (e) {
-            console.log("Got error: " + e.message);
-        });
+                var parsedData = JSON.parse(data);
+                var majesticSearchResults = [];
 
-        var getMajesticDataSuccess = function (response) {
-            var parsedData = JSON.parse(response);
-            if (parsedData["DataTables"]["Results"]["Data"].length > 0) {
-                for (var i = 0; i < parsedData["DataTables"]["Results"]["Data"].length; i++) {
-                    majesticSearchResults.push({
-                        url: parsedData["DataTables"]["Results"]["Data"][i]["Item"],
-                        title: parsedData["DataTables"]["Results"]["Data"][i]["Title"],
-                        trustFlow: parsedData["DataTables"]["Results"]["Data"][i]["TrustFlow"]
+                if (parsedData["DataTables"]["Results"]["Data"].length > 0) {
+                    for (var i = 0; i < parsedData["DataTables"]["Results"]["Data"].length; i++) {
+                        majesticSearchResults.push({
+                            url: parsedData["DataTables"]["Results"]["Data"][i]["Item"],
+                            title: parsedData["DataTables"]["Results"]["Data"][i]["Title"],
+                            trustFlow: parsedData["DataTables"]["Results"]["Data"][i]["TrustFlow"]
+                        });
+                    }
+
+                    // order by Trust Flow
+                    majesticSearchResults.sort(function (a, b) {
+                        if (a.trustFlow > b.trustFlow) {
+                            return -1;
+                        }
+                        if (a.trustFlow < b.trustFlow) {
+                            return 1;
+                        }
+                        // a must be equal to b
+                        return 0;
                     });
                 }
 
-                // order by Trust Flow
-                majesticSearchResults.sort(function (a, b) {
-                    if (a.trustFlow > b.trustFlow) {
-                        return -1;
-                    }
-                    if (a.trustFlow < b.trustFlow) {
-                        return 1;
-                    }
-                    // a must be equal to b
-                    return 0;
-                });
-            }
-        }
+                resolve(majesticSearchResults);
+            });
+        }).on('error', function (e) {
+            console.log("Got error: " + e.message);
+            reject(e);
+        });
+    });
+}
 
+function getYoutubePromise(query)
+{
+    return new Promise(function(resolve, reject) {
         youTube.search(query, 1, function (error, result) {
             if (error) {
                 console.log(error);
+                reject(error);
             } else {
                 var jsonResult = JSON.stringify(result);
                 var videoResults = JSON.parse(jsonResult);
+                var youtubeResults = [];
+                
                 if (videoResults["items"].length > 0) {
                     var video = {
                         "id": videoResults["items"][0]["id"]["videoId"],
                         "title": videoResults["items"][0]["snippet"]["title"]
                     };
+                    
                     youtubeResults.push(video);
                 }
+                
+                resolve(youtubeResults);
             }
         });
+    });
+}
 
+function getGiphyPromise(query)
+{
+    return new Promise(function(resolve, reject) {
+        var gifSearch = query.replace(/ /g, '+');
+        var numGifResults = 6;
+        
         // do a giphy search
         http.get("http://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=dc6zaTOxFJmzC&limit=" + numGifResults + "&fmt=json", function (res) {
             // add gif results to gifResults
@@ -150,29 +193,20 @@ module.exports = {
             });
 
             res.on('end', function () {
-                gifSearchSuccess(data);
-            });
-        }).on('error', function (e) {
-            console.log("Got error: " + e.message);
-        });
+                var results = JSON.parse(data);
+                var gifResults = [];
 
-        var gifSearchSuccess = function (response) {
-            var results = JSON.parse(response);
-
-            if (results["data"].length > 0) {
                 for (var i = 0; i < results["data"].length; i++) {
                     gifResults.push({
                         url: results["data"][i]["images"]["original"]["url"]
                     });
                 }
-            }
 
-            return callback({
-                gifResults: gifResults,
-                majesticSearchResults: majesticSearchResults,
-                sponsoredResults: sponsoredGifResults,
-                youtubeResults: youtubeResults
+                resolve(gifResults);
             });
-        }
-    }
+        }).on('error', function (e) {
+            console.log("Got error: " + e.message);
+            reject(e);
+        });
+    });
 }
